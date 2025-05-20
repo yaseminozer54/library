90cacdfa6e982a60597d78f7e7b721518f2f5197
yaseminozer54 <yaseminzerr@gmail.com>
2025-05-20 17:31:15 +0300

    Initial commit

---
+++ src/main/java/com/library/library/LibraryController.java
@@ -0,0 +1,13 @@
+package com.library.library;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Library Management System is running!";
+    }
+}
